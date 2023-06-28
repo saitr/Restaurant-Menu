0a1eb547efcc43119ce07917748cfbc875a43a9d
Sai <timma.reddy@dollarbirdinc.com>
2023-06-28 14:18:14 +0530

    deleted the file

--- restaurants/settings/base.py
+++ restaurants/settings/base.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'restaurants.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -135,6 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
